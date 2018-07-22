811a551bc1c868ba8f4337d0a60cedd7a6a6f401
Anderson <anderlentz@gmail.com>
2018-07-21 23:44:13 -0300

    First commit

---
+++ listing3-1.py
@@ -0,0 +1,61 @@
+"""
+    Binary classification: Classify movie reviews as positive or negativE
+    Used IMDB dataset from keras
+    This exercise is from the book Deep Learning with Python, page 68
+
+
+
+"""
+
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+import numpy as np
+
+(train_data,train_labels),(test_data, test_labels) = imdb.load_data(num_words = 10000)
+
+word_index = imdb.get_word_index()
+
+reverse_word_index = dict([(value,key) for (key,value) in word_index.items()])
+
+decoded_review = ''.join([reverse_word_index.get(i-3,'?') for i in train_data[0]])
+
+#Encoding the integer sequences into a binary matrix
+def vectorize_sequences(sequences,dimension=10000):
+    results = np.zeros((len(sequences),dimension))
+    for i,sequence in enumerate(sequences):
+        results[i,sequence] = 1.
+    return results
+
+
+#vectorized training and test data
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+
+# ----- The model definition ---------
+
+"""
+    The argument passed to each Denser layer (16) is the number of hidden units of the layer.
+        - hidden unit: is a dimension in the representation space of the layer
+        - you can understand the dimensionality of your representation space as "how much freedom you're allowing the
+          network to have when learning internal representations"
+        - having more hidden units allows your network to learn more complex representations, but it makes the network
+          computationally expensive and may lead to learning unwanted patterns  
+        
+    relu activation implements the following chain of tensor operations:   
+        output = relu((doc(W,input)+b)
+
+"""
+
+model = models.Sequential()
+model.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16,activation='relu'))
+
+# the final layer uses sigmoid activation to output a probability
+model.add(layers.Dense(1,activation='sigmoid'))
+
+
